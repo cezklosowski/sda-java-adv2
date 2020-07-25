78bf0943817d32e93aefec023ccf82419f85f446
cezklosowski <cez.klosowski@gmail.com>
2020-07-25 13:38:38 +0200

    Exercise 7 MultithreadApp v2

--- src/main/java/Ex07/MultithreadApp.java
+++ src/main/java/Ex07/MultithreadApp.java
@@ -4,16 +4,31 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class MultithreadApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*
         Zrób nową klasę MultithreadApp z metodą main oraz metodą obliczającą silnię z 5000 i drukującą każdy kolejny krok przy mnożeniu. Użyj klasy BigInteger do przechowania danych
          */
 
-        /*        Thread threadOne = new Thread();
-        threadOne.start();
-        */
+        // utworzenie wątków
+        Thread threadOne = new Thread(() -> factorialWorker(5000));
+        Thread threadTwo = new Thread(() -> factorialWorker(5000));
+        Thread threadThree = new Thread(() -> factorialWorker(5000));
+        Thread threadFour = new Thread(() -> factorialWorker(5000));
+
         long start = System.currentTimeMillis();
-        factorialWorker(5000);
+
+        // uruchomienie wątków
+        threadOne.start();
+        threadTwo.start();
+        threadThree.start();
+        threadFour.start();
+
+        // poczekanie aż się wątki wykonają
+        threadOne.join();
+        threadTwo.join();
+        threadThree.join();
+        threadFour.join();
+
         System.out.println("Finished in: " + (System.currentTimeMillis() - start) + " ms");
 
 
