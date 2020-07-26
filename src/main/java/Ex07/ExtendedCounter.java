809a85826f46badf993936ee74c9b39dc5d91fc2
cezklosowski <cez.klosowski@gmail.com>
2020-07-26 13:54:36 +0200

    Exercise 7 ExtendedCounter

---
+++ src/main/java/Ex07/ExtendedCounter.java
@@ -0,0 +1,42 @@
+package Ex07;
+
+public class ExtendedCounter {
+
+    private int countOne;
+    private int countTwo;
+
+    private final Object flagForCountOne = new Object();
+    private final Object flagForCountTwo = new Object();
+
+    public void incrementCountOne() {
+        synchronized (flagForCountOne) {
+            countOne++;
+        }
+    }
+
+    public void incrementCountTwo() {
+        synchronized (flagForCountTwo) {
+            countTwo++;
+        }
+    }
+
+    public void decrementCountOne() {
+        synchronized (flagForCountOne) {
+            countOne--;
+        }
+    }
+
+    public void decrementCountTwo() {
+        synchronized (flagForCountTwo) {
+            countTwo--;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ExtendedCounter{" +
+                "CountOne=" + countOne +
+                ", CountTwo=" + countTwo +
+                '}';
+    }
+}
